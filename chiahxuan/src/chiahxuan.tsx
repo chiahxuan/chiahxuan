d285137bb40eafa6de745b7a1541d3aaf7ae925f
chiahxuan <chiahxuan22@gmail.com>
2020-12-24 13:38:43 +0800

    initialte main page

---
+++ chiahxuan/src/chiahxuan.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function chiahxuan() {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h1>Chiah Xuan</h1>
+                    <p>Hello my name is Chiah Xuan. I am from Malaysia. I am always ready to stonks.</p>
+                </div>
+            </section>
+        </div>
+    );
+}
